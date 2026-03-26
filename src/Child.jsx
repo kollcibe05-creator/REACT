729153d233966576b750502b7fbdcdfd68cadd96
Collins Kibet <kollcibe05@gmail.com>
2026-03-26 10:34:00 +0300

    tHiSiStEsting

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 function Child (props) {
-    const {children, persona} = props
+    const [switchState, setSwitch] = useState(false)
+    function handleClick () { setSwitch((switchState) => !switchState)}
+    console.log(Math.floor(Math.random()* 11) )
     return (
         <>
         <h1>Child: What is on your mind man.</h1>
-        <p>{persona.name} is {persona.age} old. He is full of  {persona.value}.</p>
-        {children}
+        {/* <p>{persona.name} is {persona.age} old. He is full of  {persona.value}.</p>
+        {children} */}
+        <button style={{background:"red"}}onClick={handleClick}>{switchState ? "ON": "OFF"}</button>
         </>
     )
 }
