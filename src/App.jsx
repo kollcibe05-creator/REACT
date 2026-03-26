d287732ab5648940de81c4e4271f79892c09c996
Collins Kibet <kollcibe05@gmail.com>
2026-03-26 23:23:28 +0300

    AdDendum

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import {format} from 'date-fns'
-import Parent from "./Parent.jsx"
-import Children from './Children.jsx'
-import UserProvider from "./Context";
+// import {format} from 'date-fns'
+// import Parent from "./Parent.jsx"
+// import Children from './Children.jsx'
+// import UserProvider from "./Context";
+// import Form from './Form.jsx'
+import Output from './OutputSearch'
 
 
 function App() {
   return (
     <>
-      <UserProvider>
-      <Parent/>
-      <Children/>
-      </UserProvider>
-
+    <Output/>
     </>
   )
 }
