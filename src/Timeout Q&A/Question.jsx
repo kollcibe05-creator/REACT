6ff83d2927c71a7339dd1904c9729ac4d77003d2
Collins Kibet <kollcibe05@gmail.com>
2026-03-31 07:28:45 +0300

    Q & A with Timeout

---
+++ src/Timeout Q&A/Question.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+
+function Question({question, onAnswered}){
+    const [timeRemaining, setTimeRemaining] = useState(10)
+
+    useEffect(() => {
+        if (timeRemaining === 0){
+            setTimeRemaining(10)
+            onAnswered(false)
+            return //added
+        }
+        const timeId = setTimeout(() => setTimeRemaining((time) => time - 1), 1000)
+        return function () {
+            clearTimeout(timeId)
+        }
+    }, [timeRemaining, onAnswered])
+    function handleAnswer(isCorrect) {
+        setTimeRemaining(10)
+        onAnswered(isCorrect)
+    }
+
+    const {id, prompt, answers, correctIndex} = question
+
+    return(
+        <>         
+            <h1>Question {id}</h1>
+            <h3>{prompt}</h3>
+            {answers.map((answer, index) => {
+                const isCorrect = index === correctIndex
+                return(
+                    <button
+                        key={answer}
+                        onClick={() => handleAnswer(isCorrect)}
+                    >
+                        {answer}    
+                    </button>
+                );
+            })}
+            <h5>{timeRemaining} seconds remaining</h5>
+        </>
+    )
+
+}
+
+export default Question
