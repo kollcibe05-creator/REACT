895b7b3231b1953331151891269cdacc9696f94b
Collins Kibet <kollcibe05@gmail.com>
2026-03-31 07:27:32 +0300

    Q & A with Timeout

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,42 @@ import './App.css'
 // import UserProvider from "./Context";
 // import Form from './Form.jsx'
 // import Output from './OutputSearch'
-import UseEffect from './UseEffect'
+// import UseEffect from './UseEffect'
+import quiz from './Timeout Q&A/quiz'
+import Question from './Timeout Q&A/Question'
 
 function App() {
+  const [questions, setQuestions] = useState(quiz)
+  const [currentQuestionId, setCurrentQuestion] = useState(1)
+  const[score, setScore] = useState(0)
+  const currentQuestion = questions.find(q => q.id === currentQuestionId)
+
+  function handleQuestionsAnswered(correct) {
+    if(currentQuestionId < questions.length) {
+      setCurrentQuestion((currentQuestion) => currentQuestionId + 1)
+    }else {
+      setCurrentQuestion(null)
+    }
+    if(correct) {
+      setScore(score => score + 1)
+    }
+  }
   return (
-    <>
-      <UseEffect/>
-    </>
+    <main>
+      <section>
+        {currentQuestion ? (
+          <Question
+            question={currentQuestion}
+            onAnswered={handleQuestionsAnswered}
+          />
+        ): (
+          <>
+            <h1>Game Over</h1>
+            <h2>Total Correct: {score}</h2>
+          </>
+        )}
+      </section>
+    </main>
   )
 }
 
