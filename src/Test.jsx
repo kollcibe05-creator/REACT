e23aa76c2d46a11cd6f51269979aedc689fadc91
Collins Kibet <kollcibe05@gmail.com>
2026-04-09 23:51:53 +0300

    written in it the test cases

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+function Test() {
+    return (
+        <div>
+            <header>
+            <h1>Well Hello There</h1>
+            <a href="https://www.youtube.com">Never Gonna Give You Up</a>
+            <button>Click Me!</button>
+            </header>
+        </div>
+    )
+}
+export default Test
