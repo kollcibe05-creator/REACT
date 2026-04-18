20c2e2eb3080a7fb4c9819ea09de81c888f74813
Collins Kibet <kollcibe05@gmail.com>
2026-04-18 14:58:25 +0300

    included <select> and <option> for testing

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 function Test() {
+    const [addPepperoni, setPepperoni] = useState(false)
+            
+    function togglePepperoni() {
+        setPepperoni(!addPepperoni)
+    }
     return (
         <div>
             <header>
@@ -6,6 +13,21 @@ function Test() {
             <a href="https://www.youtube.com">Never Gonna Give You Up</a>
             <button>Click Me!</button>
             </header>
+            <h1>Select Pizza Toppings</h1>
+            <input 
+            type="checkbox"  
+            id="pepperoni" 
+            checked={addPepperoni} 
+            aria-checked={addPepperoni}
+            onChange={togglePepperoni}
+            />
+            <label htmlFor="pepperoni">Add pepperoni</label>
+            <h2>Your Toppings</h2>
+            <ul>
+                <li>Cheese</li>
+                {addPepperoni ? <li>Pepperoni</li> : null}
+            </ul>
+
         </div>
     )
 }
